b7327f50928630eaac92ad5eb48902045c203cdf
Itamar Tsayag <itamartsayag@ip-10-0-0-11.us-west-2.compute.internal>
2021-02-28 10:48:49 +0200

    --null

--- download_query_csv_scans.py
+++ download_query_csv_scans.py
@@ -2,23 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
+import os
 
 
 class DownloadFromCSV(object):
-    def __init__(self, csv_path):
+    def __init__(self, csv_path, output_path):
         self.csv_path = csv_path
-        self.df = pd.read_csv(self.csv_pth)
+        self.output_path = output_path
+        self.df = self.read_csv()
 
     def read_csv(self):
-        pass
+        return pd.read_csv(self.csv_path)
 
-    def download(self):
-        pass
+    def download(self, n_rows=3):
+        for row in self.df.head(n_rows).iterrows():
+            viz_patient_id, study_uid, series_uid = row[1]['viz_patient_id'], row[1]['study_uid'], row[1]['series_uid']
+            cmd = self.dwnld_cmd(viz_patient_id, study_uid, series_uid)
+            self.exec_cmd(cmd)
+
+    def dwnld_cmd(self, viz_patient_id, study_uid, series_uid):
+        cmd = f'aws s3 sync s3://viz-rnd-us-east-2-deidentification-results/production/{viz_patient_id}/{study_uid}/{series_uid}/' \
+              f' {os.path.join(self.output_path, viz_patient_id, study_uid, series_uid)}'
+        return cmd
+
+    @staticmethod
+    def exec_cmd(cmd):
+        print(f'Executing: {cmd}')
+        os.system(cmd)
+
 
 
 def main():
     csv_path = '/Users/itamartsayag/workspace/tasks/jog_mode_enabelment/viz_etl.csv'
-    dwn = DownloadFromCSV(csv_path)
+    output_path = '/Users/itamartsayag/workspace/data/ctp/dicoms'
+    dwn = DownloadFromCSV(csv_path, output_path)
+    dwn.download(7)
 
 if __name__ == '__main__':
     main()
